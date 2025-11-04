2599f4e81df20483630dbcf4429d0ee6808d1ae2
isaacfavel <pirrisfavela@gmail.com>
2025-11-04 15:51:11 -0600

    funcional

--- MauiApp02/MainPage.xaml.cs
+++ MauiApp02/MainPage.xaml.cs
@@ -16,8 +16,8 @@
 
                 int numwero1 = int.Parse(txtNumero1.Text);
                 int numwero2 = int.Parse(txtNumero1.Text);
-                int suma = numwero1 + numwero2;
-                lblResultado.Text = $"Resultado: {suma}";
+                int resultado = numwero1 + numwero2;
+                lblResultado.Text = $"Resultado: {resultado}";
             }
             catch (Exception ex) { 
             
@@ -25,5 +25,58 @@
 
             
         }
+
+        private void OnRestarClicked(object? sender, EventArgs e)
+        {
+            try
+            {
+
+                int numwero1 = int.Parse(txtNumero1.Text);
+                int numwero2 = int.Parse(txtNumero1.Text);
+                int resultado = numwero1 - numwero2;
+                lblResultado.Text = $"Resultado: {resultado}";
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+        private void OnMultiplicarClicked(object? sender, EventArgs e)
+        {
+            try
+            {
+
+                int numwero1 = int.Parse(txtNumero1.Text);
+                int numwero2 = int.Parse(txtNumero1.Text);
+                int resultado = numwero1 * numwero2;
+                lblResultado.Text = $"Resultado: {resultado}";
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+        private void OnDividirClicked(object? sender, EventArgs e)
+        {
+            try
+            {
+
+                int numwero1 = int.Parse(txtNumero1.Text);
+                int numwero2 = int.Parse(txtNumero1.Text);
+                int resultado = numwero1 / numwero2;
+                lblResultado.Text = $"Resultado: {resultado}";
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+        
     }
 }
